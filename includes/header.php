d17eb63a5230baa3756e7b592edbd28d845ad965
kenser <koffy140221@gmail.com>
2023-05-31 10:26:13 -0600

    modificar estilo de fondo

--- includes/header.php
+++ includes/header.php
@@ -6,11 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-        body{
-            /* background-image: url('/practica_08/asset/images/fondo.jpg') !important;
-            background-size:cover; */
+
+        /*aqui se diseña el cuerpo de la pagina*/
+
+        body {
+            background-image: url(./fondo.jpg);
+            background-size:cover;
+            background-attachment: fixed;
 
         }
+
+        /*diseño del tipo de texto h1*/
+        h1 {
+            color: rgba(255, 255, 255, 0.948);
+            font-family: 'ALGERIAN';
+            text-shadow:2px 2px 1px rgb(1, 1, 1);
+        
+        }
+        /*diseño del tipo de texto h2*/
+        h2 {
+            color: rgb(0, 0, 0);
+            text-align: center;
+            text-shadow: 2px 2px 2px whitesmoke;
+            border-color: azure;
+            border-radius: 4px;
+            font-family: 'calibri';
+            background: rgba(204, 202, 204, 0.705);
+            opacity: 0.7;
+            box-shadow: 1px 1px 10px gold;
+        }
+        /*diseño del contenedor*/
+        .container {
+
+            justify-content: center;
+            width: 800px;
+            margin-top: 15px;
+            margin-left: 350px;
+        }
+        #alert{
+            margin-left:60px;
+        }
+
     </style>
     <title>App de Tienda</title>
 </head>
