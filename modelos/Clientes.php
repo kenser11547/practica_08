f6bde2258449b601b26f7a77a94d4f05f5cc41d2
kenser <koffy140221@gmail.com>
2023-05-31 10:00:57 -0600

    a

--- modelos/Clientes.php
+++ modelos/Clientes.php
@@ -113,6 +113,37 @@ class Cliente extends Conexion{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados</title>
+    <style>
+    body {
+        background-image: url(./fondo.jpg);
+        background-size:cover;
+        background-attachment: fixed;
+    }
+    /*diseño del tipo de texto h1*/
+    h1 {
+        color: rgba(255, 255, 255, 0.948);
+        font-family: 'ALGERIAN';
+        text-shadow:2px 2px 1px rgb(1, 1, 1);
+    
+    }
+    /*diseño del tipo de texto h2*/
+    h2 {
+        color: rgb(0, 0, 0);
+        text-align: center;
+        text-shadow: 2px 2px 2px whitesmoke;
+        border-color: azure;
+        border-radius: 4px;
+        font-family: 'calibri';
+        background: rgba(204, 202, 204, 0.705);
+        opacity: 0.7;
+        box-shadow: 1px 1px 10px gold;
+    }
+    /*diseño del contenedor*/
+    .container {
+        justify-content: center;
+        width: 600px;
+    }
+    </style>
 </head>
 <body>
     <div class="container">
