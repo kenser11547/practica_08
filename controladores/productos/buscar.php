e5bf9f2d5d529ebf8ca41f593b067350f9328342
kenser <koffy140221@gmail.com>
2023-05-31 10:05:55 -0600

    cambios3

--- controladores/productos/buscar.php
+++ controladores/productos/buscar.php
@@ -24,6 +24,53 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados</title>
+    <style>
+                * {
+            margin: 25;
+            padding: 5;
+            box-sizing: border-box;
+        }
+        /*aqui se diseña el cuerpo de la pagina*/
+
+        body {
+            background-image: url(./fondo.jpg);
+            background-size:cover;
+            background-attachment: fixed;
+
+        }
+
+        /*diseño del tipo de texto h1*/
+        h1 {
+            color: rgba(255, 255, 255, 0.948);
+            font-family: 'ALGERIAN';
+            text-shadow:2px 2px 1px rgb(1, 1, 1);
+        
+        }
+        /*diseño del tipo de texto h2*/
+        h2 {
+            color: rgb(0, 0, 0);
+            text-align: center;
+            text-shadow: 2px 2px 2px whitesmoke;
+            border-color: azure;
+            border-radius: 4px;
+            font-family: 'calibri';
+            background: rgba(204, 202, 204, 0.705);
+            opacity: 0.7;
+            box-shadow: 1px 1px 10px gold;
+        }
+        /*diseño del contenedor*/
+        .container {
+
+            justify-content: center;
+            width: 600px;
+            margin-top: 75px;
+            margin-left: 650px;
+        }
+        #alert{
+            margin-left:60px;
+        }
+
+    </style>
 </head>
 <body>
     <div class="container">
